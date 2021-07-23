afcc7606f85ac68e7824cf0f02687654eb679d53
Valerii Amitov <valeriyamitov@gmail.com>
2021-07-23 23:26:47 +0300

    exs1/Day2

---
+++ Week3/Day2/exercise1.js
@@ -0,0 +1,13 @@
+5 + "34"            //the result is: 534 because number + string 
+5 - "4"             //the result is: 1 because we cannot digital "-" strings, so 5 - 4 = 1
+10 % 5              //the result is: 0, because there is no remainder to return, because 10/5 =2
+5 % 10              //the result is 5, because the remainder of 5/10 = 0,5 is 5
+"Java" + "Script"   //the result is JavaScript because these is a sum of two strings
+" " + " "           //the result is "  " because we have a space like stroke 
+" " + 0             //the result is " 0", because 0 is empty
+true + true         // it's 1 because this is a boolean operation we have 
+true + false        // it's 1 because this is a boolean operation. If we have "true" in this operation and method "+" the answer will be "1" true
+false + true        // it's the same as on line 9
+false - true        //like in mathematic we have -1, that's the answer. `because true is 1, false is 0
+3 - 4               // the answer is -1, because it's digits operation
+"Bob" - "bill"      //it's NaN, because it's a error operation. We cannot do that with strings, only with digits
