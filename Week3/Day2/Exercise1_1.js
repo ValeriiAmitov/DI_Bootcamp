ccebb59458a36dadb521cf34dab22505e09308ca
Valerii Amitov <valeriyamitov@gmail.com>
2021-07-24 19:18:31 +0300

    Exercise1_1

---
+++ Week3/Day2/Exercise1_1.js
@@ -0,0 +1,7 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift(); //removes "Banana" from fruits
+fruits.sort(); //it's sorting our fruits//-----------------------------------------------------------//
+fruits.push("Kiwi"); //Adds "Kiwi" to fruits
+fruits.splice(0, 1); //Remove "Apple" from fruits//-----------------------------------------------------------//
+fruits.reverse(); //Reverse our fruits
+console.log(fruits);
