65d5f4d8e1f6ac0696a9f6394a29ce988de85e3e
Valerii Amitov <valeriyamitov@gmail.com>
2021-07-24 22:31:57 +0300

    Exercise_1

---
+++ Week3/Day3/Exercise_1.js
@@ -0,0 +1,9 @@
+let age = prompt("How old are you?")
+
+if (age == 18) {
+   alert("Congratulations on your first year of driving. Enjoy the ride!")
+ } else if (age > 18) {
+        alert("Powering On. Enjoy the ride!")
+    } else {
+        alert("Sorry, you are too young to drive this car. Powering off")
+    };
