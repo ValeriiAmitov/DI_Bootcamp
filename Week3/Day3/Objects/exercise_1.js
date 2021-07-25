90ba94f54112c082ab5439a33ad5517b919e8ec9
Valerii Amitov <valeriyamitov@gmail.com>
2021-07-25 15:48:11 +0300

    Day3 exercise

---
+++ Week3/Day3/Objects/exercise_1.js
@@ -0,0 +1,7 @@
+
+let user = {
+    username: "Valerii",
+    password: "devins",
+};
+var database = [user];
+var newsfeed= [{username:"John",timeline:"welcome"}, {username:"Steve",timeline:"welcome back"}, {username:"Mary",timeline:"welcome to the jungle"}];
