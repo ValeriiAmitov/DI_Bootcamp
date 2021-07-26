db7ac4c85f4f9410974d4d0a73f32bf0bf491a16
Valerii Amitov <valeriyamitov@gmail.com>
2021-07-26 13:44:09 +0300

    Exercise2

---
+++ Week3/Day4/Exercise2.js
@@ -0,0 +1,12 @@
+var newDog = "Chihuahua";
+newDog.length
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+///////////////////
+
+if (newDog == "Chihuahua") {
+    console.log("I love Chihuahuas, it’s my favorite dog breed");
+    } else {
+    console.log("I dont care, I prefer cats");
+    }
