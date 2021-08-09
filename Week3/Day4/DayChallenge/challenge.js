8c2e06b97dcac6cd0507532f8cf33009c71a5a78
Valerii Amitov <valeriyamitov@gmail.com>
2021-08-09 21:36:37 +0300

    daily day 4

--- Week3/Day4/DayChallenge/challenge.js
+++ Week3/Day4/DayChallenge/challenge.js
@@ -1,11 +1,11 @@
 var sentence = ("The chicken is not that bad, I like it");
-var wordNot = sentence.substring(15, 18);
-var wordBad = sentence.substring(24, 27);
-let str = sentence.indexOf(wordNot);
-if(str > -1){
-    sentence = ("The chicken is good, I like it");
+var wordNot = sentence.indexOf("not");
+var wordBad = sentence.indexOf("bad");
+
+if (sentence.includes("bad") && sentence.includes("not") && wordBad > wordNot) {
+    var str = sentence.substring(wordNot,wordBad+3);
+    console.log(sentence.replace(str, "good"));
+}
+else {
     console.log(sentence);
-   
-} else {
-    console.log(sentnce);
 }
