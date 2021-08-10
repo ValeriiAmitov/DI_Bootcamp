5e9ef830ba04b32ebf7919f89615223196b67675
Valerii Amitov <valeriyamitov@gmail.com>
2021-08-10 19:26:58 +0300

    exercises in class

---
+++ Week6/Day3/exercises_class/exs1.py
@@ -0,0 +1,13 @@
+#exercise1
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+list1[list1.index(int("20"))]=200
+print(list1)
+
+#exercise2
+aTuple = (10, 20, 30, 40)
+a, b, c, d = aTuple
+print(a) #10
+print(b)#20
+print(c)#30
+print(d)#40
