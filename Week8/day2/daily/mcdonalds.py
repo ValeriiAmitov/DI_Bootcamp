ee8084cbb089e7f498bea6e8d92273cc5b40bbb3
Valerii Amitov <valeriyamitov@gmail.com>
2021-08-24 18:01:35 +0300

    daily MCDonald

--- Week8/day2/daily/mcdonalds.py
+++ Week8/day2/daily/mcdonalds.py
@@ -1,20 +1,40 @@
+#MCDonald's farm
+
 class Farm:
-	def __init__(self, name):
-		self.name = name
-		self.animals = {}
+    def __init__(self, farm_name):
+        self.name = farm_name
+        self.animals = {}
 
-	def farm_name(self):
-		print(f"{self.name}'s farm")
+    def farm_name(self):
+        print(f"{self.name}'s farm")
 
-	def add_animal(self, animal, quantity):
-		self.animals[animal] = quantity
-		print(f"{animal} : {self.animals[animal]}")
+    def add_animal(self, animal, quantity=1):
+        if animal in self.animals:
+            self.animals[animal] += 1
+        else:
+            self.animals[animal] = quantity
 
-	def show_all_the_animals(self):
-		for animal, quantity in self.animals.items():
-			print(f"there is {quantity} {animal}'s")
+    def get_info(self):
+        for animal, quantity in self.animals.items():
+            print(f"{animal}: {quantity}")
 
-mcdonalds = Farm("Mcdonald")
-mcdonalds.add_animal("cow", 5)
+    def get_animals_types(self):
+        animals_list = [animals for animals in self.animals.keys()]
+        sorted_animals = sorted(animals_list)
+        return sorted_animals
 
-mcdonalds.show_all_the_animals()
+    def get_short_info(self):
+        print(f"{self.name}'s farm has:")
+        for animal in self.get_animals_types():
+            print(animal)
+
+
+macdonald = Farm("McDonald")
+macdonald.farm_name()
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
+print(macdonald.get_animals_types())
+macdonald.get_short_info()
